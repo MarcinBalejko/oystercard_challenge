e1db24a120f7f6ac610abfe93ea985c2573a5598
MarcinBalejko <mbbm9@o2.pl>
2019-05-21 01:24:48 +0800

    :recycle: string interpolation

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -5,7 +5,7 @@ class Oystercard
         @balance = 0
     end
     def top_up(amount)
-        fail 'Maximum balance of 20 exceeded' if amount + balance > MAXIMUM_BALANCE
+        fail "Maximum balance of #{MAXIMUM_BALANCE} exceeded" if amount + balance > MAXIMUM_BALANCE
         @balance += amount
     end
 end
