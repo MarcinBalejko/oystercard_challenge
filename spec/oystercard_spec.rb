784ee457ba096cbe2677aab30844bbd5448a7764
MarcinBalejko <mbbm9@o2.pl>
2019-05-21 19:06:09 +0800

    :recycle: tests refactored

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,8 +5,6 @@ describe Oystercard do
     end
 
     describe '#top_up' do
-        it { is_expected.to respond_to(:top_up).with(1).argument }
-
         it 'can top up the balance' do
             expect{ subject.top_up 1}.to change{ subject.balance }.by 1
         end 
@@ -19,8 +17,6 @@ describe Oystercard do
     end
 
     describe '#deduct' do
-        it {is_expected.to respond_to(:deduct).with(1).argument }
-
         it 'deducts an amount from the balance' do
             subject.top_up(20)
             expect{ subject.deduct 3 }.to change{ subject.balance }.by -3
