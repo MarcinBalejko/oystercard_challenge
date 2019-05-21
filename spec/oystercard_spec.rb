80b3c15c93f1bc7f1d2d7099006885d2c5db27c8
MarcinBalejko <mbbm9@o2.pl>
2019-05-21 19:03:44 +0800

    :white_check_mark: deducts an amount from the balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -18,4 +18,13 @@ describe Oystercard do
         expect{ subject.top_up 1 }.to raise_error "Maximum balance of #{maximum_balance} exceeded"
     end
 
+    describe '#deduct' do
+        it {is_expected.to respond_to(:deduct).with(1).argument }
+
+        it 'deducts an amount from the balance' do
+            subject.top_up(20)
+            expect{ subject.deduct 3 }.to change{ subject.balance }.by -3
+        end
+    end
+
 end
