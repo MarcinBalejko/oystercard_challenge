3563d65221493f1905956e8571f0ae9e8b1142b5
MarcinBalejko <mbbm9@o2.pl>
2019-05-21 22:44:07 +0800

    :recycle: Tests

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,8 +1,10 @@
 class Oystercard
-    attr_accessor :maximum_balance, :balance, :in_journey
+    attr_accessor :maximum_balance, :minimum_balance, :balance, :in_journey
     MAXIMUM_BALANCE = 20
-    def initialize(maximum_balance = MAXIMUM_BALANCE)
+    MINIMUM_BALANCE = 1
+    def initialize(maximum_balance = MAXIMUM_BALANCE, minimum_balance = MINIMUM_BALANCE)
         @maximum_balance = maximum_balance
+        @minimum_balance = minimum_balance
         @balance = 0
         @in_journey = false
     end
@@ -17,7 +19,7 @@ class Oystercard
         @in_journey
     end
     def touch_in
-        fail 'Insufficient balance to touch in' if balance < 1
+        fail 'Insufficient balance to touch in' if balance < MINIMUM_BALANCE
         @in_journey = true
     end
     def touch_out
