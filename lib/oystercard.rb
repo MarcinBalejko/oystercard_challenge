bd371db52df349788d66ec1ecba9d2c3422be703
MarcinBalejko <mbbm9@o2.pl>
2019-05-21 19:19:00 +0800

    :white_check_mark: initially not in journey

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -3,6 +3,7 @@ class Oystercard
     MAXIMUM_BALANCE = 20
     def initialize
         @balance = 0
+        @in_journey = false
     end
     def top_up(amount)
         fail "Maximum balance of #{MAXIMUM_BALANCE} exceeded" if amount + balance > MAXIMUM_BALANCE
@@ -10,5 +11,8 @@ class Oystercard
     end
     def deduct(amount)
         @balance -= amount
+    end
+    def in_journey?
+        @in_journey
     end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -23,4 +23,10 @@ describe Oystercard do
         end
     end
 
+    describe '#in_journey?' do
+        it 'initially not in journey' do
+            expect(subject).not_to be_in_journey
+        end
+    end
+
 end
