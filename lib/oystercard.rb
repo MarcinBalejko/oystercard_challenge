c44c98b4051cc8e15bdc936db3551096a18e6d8c
MarcinBalejko <mbbm9@o2.pl>
2019-05-22 17:02:32 +0800

    :fire: in_journey instance variable removed

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,6 +1,6 @@
 require_relative 'station'
 class Oystercard
-    attr_accessor :maximum_balance, :minimum_balance, :minimum_charge, :entry_station, :balance, :in_journey
+    attr_accessor :maximum_balance, :minimum_balance, :minimum_charge, :entry_station, :balance
     MAXIMUM_BALANCE = 20
     MINIMUM_BALANCE = 1
     MINIMUM_CHARGE = 1
@@ -10,23 +10,22 @@ class Oystercard
         @minimum_charge = minimum_charge
         @entry_station = entry_station
         @balance = 0
-        @in_journey = false
     end
     def top_up(amount)
         fail "Maximum balance of #{MAXIMUM_BALANCE} exceeded" if amount + balance > MAXIMUM_BALANCE
         @balance += amount
     end
     def in_journey?
-        @in_journey
+        @entry_station
     end
     def touch_in(station)
         fail 'Insufficient balance to touch in' if balance < MINIMUM_BALANCE
         @entry_station = station
-        @in_journey = true
+        
     end
     def touch_out
         deduct(MINIMUM_CHARGE)
-        @in_journey = false
+        @entry_station = nil
     end
 
     private
