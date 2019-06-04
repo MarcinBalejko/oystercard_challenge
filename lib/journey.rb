b3d95d1f72da05f4ddb9ececcfc077b5f7e91428
MarcinBalejko <mbbm9@o2.pl>
2019-06-04 16:16:06 +0800

    :white_check_mark: Journey stores entry and exit stations

--- lib/journey.rb
+++ lib/journey.rb
@@ -2,9 +2,8 @@ require_relative 'oystercard'
 require_relative 'station'
 class Journey
     attr_accessor :entry_station, :exit_station, :current_journeys, :list_of_journeys
-    MINIMUM_CHARGE = 1
-    def initialize(minimum_charge = MINIMUM_CHARGE)
-        @current_journeys = []    #journeY
+    def initialize
+        @current_journeys = []    
         @list_of_journeys = {}
         @entry_station = entry_station
         @exit_station = exit_station
@@ -13,9 +12,11 @@ class Journey
         @current_journeys[-1]
     end
     def start(entry_station)
+        @entry_station = entry_station
         @current_journeys << entry_station
     end
     def finish(exit_station)
+        @exit_station = exit_station
         @current_journeys << exit_station
         @list_of_journeys[@current_journeys] =  @list_of_journeys.count + 1
         @current_journeys = [] 
@@ -23,9 +24,11 @@ class Journey
     def complete?
         return false if @current_journeys[1] == nil   
     end
-
-
-
+    #def verify_journey
+     # whether no entry x2
+     # 
+    #end
+    
     #def finish
     #end
     #def complete?
