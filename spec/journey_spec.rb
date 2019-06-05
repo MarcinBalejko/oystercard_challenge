35c75ce2a089d3deaaf4076b45fdb858b3d945f3
MarcinBalejko <mbbm9@o2.pl>
2019-06-06 00:57:59 +0800

    :recycle: Added context when journey started

--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -16,36 +16,35 @@ describe Journey do
         end
     end
 
-    it 'stores the entry station' do
-        subject.start(entry_station)
-        expect(subject.current_journeys[0]).to eq entry_station
-    end
-
-    it 'stores exit station' do
-        subject.start(entry_station)
-        subject.finish(exit_station)
-        expect(subject.list_of_journeys.keys[-1][-1]).to eq(exit_station)
-    end
-
-    it 'has an empty list of journeys by default' do
-        expect(subject.list_of_journeys).to be_empty
-    end
-
-    describe '#complete?' do
-        it 'returns false if the journey is not complete' do
+    context 'when journey started' do
+        before do
             subject.start(entry_station)
-            expect(subject.complete?).to eq(false)
+        end
+
+        it 'stores the entry station' do
+            expect(subject.current_journeys[0]).to eq entry_station
+        end
+
+        it 'stores exit station' do
+            subject.finish(exit_station)
+            expect(subject.list_of_journeys.keys[-1][-1]).to eq(exit_station)
+        end
+
+        it 'has an empty list of journeys by default' do
+            expect(subject.list_of_journeys).to be_empty
+        end
+
+        describe '#complete?' do
+            it 'returns false if the journey is not complete' do
+                expect(subject.complete?).to eq(false)
+            end
+        end
+
+        describe '#calculate_fare' do
+            it 'calculates the fare according to the zones' do
+                expect(subject.calculate_fare(exit_station)).to eq(2)         
+            end
         end
     end
-
-    describe '#calculate_fare' do
-        it 'calculates the fare according to the zones' do
-            subject.start(entry_station)
-            expect(subject.calculate_fare(exit_station)).to eq(2)         
-        end
-    end
-
-
-
 
 end
